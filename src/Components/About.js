64d1bde3ca371e8e4fe683f83399192d511aa3ca
asadraiyan <asadraiyan001@gmail.com>
2023-03-21 23:57:19 +0530

    created contact page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AboutIntroimg from '../assets/about-img1.png'
+import { Link } from "react-router-dom";
 
 const About = () => {
     return (
@@ -7,7 +8,7 @@ const About = () => {
             <div className="heading-container">
                 <h1 className='about-heading'>About Us</h1>
                 <p className='about-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ipsa officia repellat necessitatibus iste velit facere, cupiditate ullam porro dolorum rem quo aut laboriosam aliquam, incidunt pariatur dicta nulla commodi natus praesentium error in! Veritatis perferendis vel voluptatibus ipsa eaque incidunt ipsum quis ad laudantium tenetur, pariatur a soluta asperiores doloremque cum commodi quos dicta nostrum quaerat voluptatem assumenda. Natus eaque beatae ipsum nisi voluptatem illo deleniti consequatur tenetur ducimus.</p>
-                <button className='contact-btn'>Contact</button>
+                <Link to="/Contact" className='contact-btn'>Contact</Link>
             </div>
             <div className="aboutimg-conatiner">
                 <img src={AboutIntroimg} alt="AboutIntroimg" className='about-img' />
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import Introcontactimg from '../assets/contact-img.png'
 
 const Contact = () => {
     return (
-        <div>
+        <>
+            <div className="big-container">
+                <div className='contact-container'>
+                    <h1 className='questions'>Have Some Questions ? </h1>
+                </div>
+            </div>
+            <div className="form-container">
+                <div className="contact-img">
+                    <img src={Introcontactimg} alt="Introcontactimg" className='img-contact' />
+                </div>
 
-        </div>
+                <form className='form-page'>
+                    <label className='label'> Your Name:</label>
+                    <input className='txt' type="text" placeholder='Enter Your Name' autoComplete='off' />
+                    <label className='label'>Email:</label>
+                    <input className='txt' type="email" placeholder='Enter Your email' autoComplete='off' />
+                    <label className='label'>Contact:</label>
+                    <input className='txt' type="text" placeholder='Enter Your Phone Number' autoComplete='off' />
+                    <label className='label'>Message:</label>
+                    <textarea className='msg-txt' rows="6" placeholder='Type your message here'></textarea>
+                    <button className='submit-btn'>Submit</button>
+                </form>
+
+            </div>
+        </>
     )
 }
 
