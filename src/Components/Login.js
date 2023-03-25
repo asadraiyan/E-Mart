8d3aee51b9d93bd29a04b96f8396ddc9f5c48e86
asadraiyan <asadraiyan001@gmail.com>
2023-03-25 17:17:24 +0530

    made responsive login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,28 +1,39 @@
 import React from 'react'
 import { AiOutlineGoogle } from "react-icons/ai";
 import { FaFacebookF } from "react-icons/fa";
+import Loginimg from '../assets/login-img.jpg'
 
 const Login = () => {
     return (
-        <div className='login-box'>
-            <div className="login-heading">
-                <h1>Login</h1>
+        <>
+            <div className="login-container">
+                <div className="login-img">
+                    <img src={Loginimg} alt="Loginimg" className='img-login' />
+                </div>
+                <div className='login-box'>
+                    <div className="login-heading">
+                        <h1 className='log'>Login</h1>
+                    </div>
+                    <div className="social-icons">
+                        <button className='google' ><AiOutlineGoogle className='googlefb-icon' /> Sign in with Google</button>
+                        <button className='facebook'><FaFacebookF className='googlefb-icon' /> Sign in with Facebook</button>
+                    </div>
+                    <div className="email-container">
+                        <label className='email-address'>Email Address</label>
+                        <input className='email' type="text" placeholder='Enter your email' />
+                        <p className='not-share'>We'll never share your email with anyone else</p>
+                    </div>
+                    <div className="password-container">
+                        <label className='password'>Password</label>
+                        <input className='pass' type="password" placeholder='Enter your password' />
+                        <div className="checkbox">
+                            <input type="checkbox" className='check-type' /><span className='check'>Check me out</span>
+                        </div>
+                    </div>
+                    <button className='submit'> Submit</button>
+                </div>
             </div>
-            <hr />
-            <div className="social-icons">
-                <button className='google' ><AiOutlineGoogle /> Sign in with Google</button>
-                <button className='facebook'><FaFacebookF /> Sign in with Facebook</button>
-            </div>
-            <label className='email-address'>Email Address</label>
-            <input className='email' type="text" placeholder='Enter your email' />
-            <p className='not-share'>We'll never share your email with anyone else</p>
-            <label className='password'>Password</label>
-            <input className='pass' type="password" placeholder='Enter your password' />
-            <div className="checkbox">
-                <input type="checkbox" className='check-type' /><span className='check'>Check me out</span>
-            </div>
-            <button className='submit'> Submit</button>
-        </div>
+        </>
     )
 }
 
