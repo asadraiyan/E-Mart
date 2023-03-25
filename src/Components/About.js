b179eaba8d50917a6094f12fe0e3ba3f79f11cf9
asadraiyan <asadraiyan001@gmail.com>
2023-03-25 12:02:13 +0530

    Made about page responsive

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,14 +5,15 @@ import { Link } from "react-router-dom";
 const About = () => {
     return (
         <div className='about-container'>
-            <div className="heading-container">
-                <h1 className='about-heading'>About Us</h1>
-                <p className='about-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ipsa officia repellat necessitatibus iste velit facere, cupiditate ullam porro dolorum rem quo aut laboriosam aliquam, incidunt pariatur dicta nulla commodi natus praesentium error in! Veritatis perferendis vel voluptatibus ipsa eaque incidunt ipsum quis ad laudantium tenetur, pariatur a soluta asperiores doloremque cum commodi quos dicta nostrum quaerat voluptatem assumenda. Natus eaque beatae ipsum nisi voluptatem illo deleniti consequatur tenetur ducimus.</p>
-                <Link to="/Contact" className='contact-btn'>Contact</Link>
-            </div>
             <div className="aboutimg-conatiner">
                 <img src={AboutIntroimg} alt="AboutIntroimg" className='about-img' />
             </div>
+            <div className="heading-container">
+                <h1 className='about-heading'>About Us</h1>
+                <p className='about-content'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+                <Link to="/Contact" className='contact-btn'>Contact</Link>
+            </div>
+
         </div>
     )
 }
